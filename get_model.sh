f347bce392955b8482bc599fe691bb72627e4c02
HP <phunghx@gmail.com>
2019-06-16 15:57:08 +0900

    get model

--- get_model.sh
+++ get_model.sh
@@ -3,5 +3,5 @@ mkdir -p data/cars/train
 mkdir -p data/cars/val
 mkdir -p models/model_detection
 mkdir -p data/Grab_Challenge
-wget --no-check-certificate https://drive.google.com/file/d/10OpJGEVOJLSxLHr7rPHC1cSyKGa9CZC1/view?usp=sharing -O models/model_detection/yolov3_car_10000.weights
-wget --no-check-certificate https://drive.google.com/file/d/1SYuy5dfU5F8oNqVkqsiCbqjZwHnoaiJP/view?usp=sharing -O models/model_best.pth
+wget --no-check-certificate https://googledrive.com/host/1N-E3tRJxxfSZcXmn82RiuMu65MulD6hP/yolov3_car_10000.weights -O models/model_detection/yolov3_car_10000.weights
+wget --no-check-certificate https://googledrive.com/host/1N-E3tRJxxfSZcXmn82RiuMu65MulD6hP/model_best.pth -O models/model_best.pth
